0db30496eb718c00128adb4e671afb286fdbe5e5
Liang Zhang <psychelzh@outlook.com>
2019-07-15 21:14:10 +0800

    Enhance coding and graphing

--- analyze_training.R
+++ analyze_training.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(readxl)
 library(extrafont)
+library(ggthemes)
 training_logs <- read_tsv("training/logs.tsv")
 users <- read_tsv("training/users.tsv")
-user_info <- read_excel("info/被试测试情况记录.xlsx") %>%
-  filter(组别 == "实验组") %>%
+user_info <- read_excel("info/所需报告名单.xlsx") %>%
   rename(name_real = 姓名) %>%
-  mutate(app_id = parse_number(paste0("2018", ID编号)))
+  mutate(app_id = parse_number(paste0("2018", ID)))
 training_data <- users %>%
   inner_join(training_logs, by = "user_id") %>%
   inner_join(user_info, by = "app_id")
@@ -19,19 +19,26 @@ plots <- training_data %>%
       ~ .x %>%
         mutate(order = row_number(game_finish_time)) %>%
         mutate(game_star = factor(game_star)) %>%
-        ggplot(aes(order, game_stage, shape = game_star, group = 1)) +
-        geom_point() +
+        ggplot(aes(order, game_stage, group = 1)) +
+        geom_point(aes(color = game_star)) +
         geom_line() +
-        labs(x = "Order", y = "Stage", shape = "Star") +
-        theme_classic(base_family = "SimHei", base_size = 18)
+        scale_color_gdocs() +
+        labs(x = "顺序", y = "关卡", color = "星星数") +
+        theme_hc(base_family = "SimHei", base_size = 12)
     )
   )
 plots %>%
-  select(-game_id, -data) %>%
-  pmap(
-    function(plot, name_real, game_name)
+  pwalk(
+    function(plot, name_real, game_name, ...) {
+      dir_fig <- file.path("reports", name_real, "training")
+      if (!dir.exists(dir_fig))
+        dir.create(dir_fig, recursive = TRUE)
       ggsave(
-        file.path("training", "figures", str_glue("{name_real}-{game_name}.jpg")),
-        plot + labs(title = str_glue("{name_real}-{game_name}")) + theme(plot.title = element_text(hjust = 0.5)), type = "cairo"
+        file.path(dir_fig, str_glue("{game_name}.png")),
+        plot +
+          labs(title = game_name) +
+          theme(plot.title = element_text(hjust = 0.5)),
+        type = "cairo", width = 6, height = 4
       )
+    }
   )
