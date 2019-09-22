3abc4990291ade64a3411c564b59d97425bd3f67
Liang Zhang <psychelzh@outlook.com>
2019-09-22 12:01:10 +0800

    Update codes to use online source of user info

--- scripts/analyze_statistics.R
+++ scripts/analyze_statistics.R
@@ -1,36 +1,58 @@
 library(tidyverse)
+library(lubridate)
+library(readxl)
 library(ggrepel)
 library(ggthemes)
-library(haven)
 library(lmerTest)
 library(emmeans)
 library(extrafont)
-conners_user_info <- read_sav("info/26人 性别年龄瑞文韦氏conners 0412年龄更新.sav") %>%
-  select(-contains("瑞文"))
-raven_scores <- read_sav("info/25人瑞文数据.sav") %>%
-  select(name, contains("瑞文")) %>%
-  add_column(item_title = "raven", .after = 1) %>%
-  rename(no = name, pre = pre瑞文, post = post瑞文) %>%
-  pivot_longer(c("pre", "post"), names_to = "time", values_to = "score")
-user_info <- conners_user_info %>%
-  select(-contains("pre"), -contains("post"))
-conners <- conners_user_info %>%
-  select(no, contains("pre"), contains("post")) %>%
-  gather(variable, score, preA行为:postF多动指数) %>%
-  separate(variable, c("time", "item_title"), sep = "[A-F]")
-all_scores <- read_tsv("test/scores.tsv") %>%
-  mutate(
-    no = parse_number(no),
-    item_title = str_extract(item_title, "\\w+")
+user_demography <- read_tsv("test/user_raw_data.tsv") %>%
+  group_by(no, gender, birthDay) %>%
+  summarise(assess_time = first(createTime)) %>%
+  ungroup() %>%
+  mutate(age = (birthDay %--% assess_time) / dyears())
+user_data_survey <- read_excel("info/被试测试情况记录.xlsx")
+user_grouping <- user_data_survey %>%
+  transmute(
+    no = ID编号,
+    group = case_when(
+      str_detect(组别, "实验组|训练") ~ "training",
+      str_detect(组别, "控制组") ~ "control",
+      TRUE ~ NA_character_
+    ),
+    IQ = parse_number(韦氏)
   ) %>%
-  select(no, item_title, time, score) %>%
-  rbind(conners) %>%
+  filter(!is.na(group))
+user_info <- user_demography %>%
+  inner_join(user_grouping, by = "no")
+write_tsv(user_info, "info/user_info.tsv")
+writexl::write_xlsx(user_info, "info/user_info.xlsx")
+raven_scores <- user_data_survey %>%
+  transmute(
+    no = ID编号,
+    item_title = "raven",
+    pre = if_else(
+      !is.na(填充测试卷别),
+      填充测试正确题数 / 填充测试完成题数,
+      前测正确数目 / 前测总完成题目
+    ),
+    post = if_else(
+      !is.na(填充测试卷别),
+      前测正确数目 / 前测总完成题目,
+      后测正确数目 / 后测总完成题目
+    )
+  ) %>%
+  filter(!is.na(pre), !is.na(post)) %>%
+  pivot_longer(c("pre", "post"), names_to = "occasion", values_to = "score")
+all_scores <- read_tsv("test/scores.tsv") %>%
+  mutate(item_title = str_extract(item_title, "\\w+")) %>%
+  select(no, item_title, occasion, score) %>%
   rbind(raven_scores)
 training_data_test <- user_info %>%
   inner_join(all_scores, by = "no") %>%
   mutate(
-    time = factor(time, levels = c("pre", "post")),
-    time_cn = factor(time, labels = c("前测", "后测")),
+    occasion = factor(occasion, levels = c("pre", "post")),
+    occasion_cn = factor(occasion, labels = c("前测", "后测")),
     group_cn = factor(
       group, levels = c("training", "control"), labels = c("训练组", "控制组")
     )
@@ -40,22 +62,23 @@ writexl::write_xlsx(training_data_test, "test/grouping_scores.xlsx")
 stats <- training_data_test %>%
   group_by(item_title) %>%
   nest() %>%
-  filter(!item_title %in% c("数感", "二维心理旋转测试", "蝴蝶照相机")) %>%
+  ungroup() %>%
+  # filter(!item_title %in% c("数感", "二维心理旋转测试", "蝴蝶照相机")) %>%
   mutate(
     fml = map(
       data,
-      ~ lmer(score ~ (group_cn + IQ + age + gender) * time_cn + (1|no), .x)
+      ~ lmer(score ~ (group_cn + IQ + age + gender) * occasion_cn + (1|no), .x)
     ),
     anova_inter = map(
       fml,
       ~ .x %>%
         anova() %>%
         broom::tidy() %>%
-        filter(term == "group_cn:time_cn")
+        filter(term == "group_cn:occasion_cn")
     ),
     plots = map(
       fml,
-      ~ emmip(.x, group_cn ~ time_cn) +
+      ~ emmip(.x, group_cn ~ occasion_cn) +
         labs(x = "", y = "预测分数", color = "") +
         scale_color_few() +
         theme_classic(base_family = "SimHei", base_size = 12) +
@@ -63,7 +86,7 @@ stats <- training_data_test %>%
     ),
     comp_plots = map(
       data,
-      ~ ggplot(.x, aes(time_cn, score, color = group_cn, group = no, label = no)) +
+      ~ ggplot(.x, aes(occasion_cn, score, color = group_cn, group = no, label = no)) +
         geom_line() +
         geom_point() +
         geom_text_repel(show.legend = FALSE) +
